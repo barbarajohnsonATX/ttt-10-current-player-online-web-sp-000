182bf13dde3072c03e343ca16d60e70bc3c3be72
Barbara Johnson <johnson.barbara@gmail.com>
2019-03-31 19:05:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
-  count = 0
-  board.each do |value|
-    if value != "" || value != " "
-      count += 1
-    end
+ # count = 0
+#  board.each do |value|
+#    if value != "" || value != " "
+#      count += 1
+#    end
 end
 
 def current_player(board)
